ee58d8a4ad7d87bd266433d74dd7134a95c2fde6
hefuweill <2995545463@qq.com>
2021-04-16 17:51:08 +0800

    add 1

---
+++ src/Q1.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q1 {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.get(nums[i]) != null) {
+                return new int[]{map.get(nums[i]), i};
+            }
+            map.put(target - nums[i], i);
+        }
+        return null;
+    }
+}
